c7147b5ba04b4a041df2fd99e565e81978a35485
ckl23 <57328826+ckl23@users.noreply.github.com>
2024-06-25 21:36:21 +0100

    Second commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require ('express')
+require('dotenv').config()
 
 // express app
-const app = expres()
+const app = express()
+
+// routes (setting up a route in an Express.js)
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to the app'})
+})
 
 // listen for requests
-app.listen (4000, () => {
-    console.log("listening on Port 4000")
+app.listen (process.env.PORT, () => {
+    console.log('listening on Port', process.env.PORT)
 });
